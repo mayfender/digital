2a4f6ebcc418cb34fa404e2df195776ec5bdf219
sarawuti <sarawut.inthong@allianz.com>
2015-10-08 17:10:56 +0700

    update

--- ricoh/src/main/java/com/may/ple/dg/ricoh/WebsocketConfig.java
+++ ricoh/src/main/java/com/may/ple/dg/ricoh/WebsocketConfig.java
@@ -1,22 +1,74 @@
 package com.may.ple.dg.ricoh;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import java.util.List;
 
-import com.may.ple.dg.ricoh.action.websocket.MyHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+import org.springframework.messaging.handler.invocation.HandlerMethodReturnValueHandler;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
-@EnableWebSocket
-public class WebsocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/myHandler");
+	}
+	
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/topic", "/queue");
+	}
+
+	@Override
+	public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureClientOutboundChannel(ChannelRegistration registration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
+		// TODO Auto-generated method stub		
+	}
+
+	@Override
+	public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+
+	/*@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 		registry.addHandler(myHandler(), "/myHandler");
-	}
+	}*/
 	
 	/*@Bean
     public ServletServerContainerFactoryBean createWebSocketContainer() {
@@ -26,9 +78,9 @@ public class WebsocketConfig implements WebSocketConfigurer {
         return container;
     }*/
 	
-	@Bean
+	/*@Bean
 	public WebSocketHandler myHandler() {
 		return new MyHandler();
-	}
+	}*/
 
 }
--- ricoh/src/main/java/com/may/ple/dg/ricoh/action/websocket/MyHandler.java
+++ ricoh/src/main/java/com/may/ple/dg/ricoh/action/websocket/MyHandler.java
@@ -1,5 +1,6 @@
 package com.may.ple.dg.ricoh.action.websocket;
 
+import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -8,7 +9,12 @@ public class MyHandler extends TextWebSocketHandler {
 	
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		System.out.println("### handleTextMessage" + message);
+		System.out.println("### handleTextMessage" + message + " " + session.getId());
+	}
+	
+	@Override
+	protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
+		System.out.println("### handleTextMessage" + message + " " + session.getId());
 	}
 	
 }
