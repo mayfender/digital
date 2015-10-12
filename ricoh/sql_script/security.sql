fb13cfec7913d4c4ce98ac03fe0b1bdf08285541
sarawuti <sarawut.inthong@allianz.com>
2015-10-12 17:22:59 +0700

    update

--- ricoh/sql_script/security.sql
+++ ricoh/sql_script/security.sql
@@ -1,7 +1,7 @@
 DROP TABLE roles;
-CREATE TABLE roles ( id int NOT NULL AUTO_INCREMENT, username varchar(45) NOT NULL, role_name varchar(45) NOT NULL, PRIMARY KEY (id) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-insert into roles (id, username, role_name) values (1, 'may', 'ROLE_USER');
-insert into roles (id, username, role_name) values (2, 'ple', 'ROLE_ADMIN');
+CREATE TABLE roles ( id int NOT NULL AUTO_INCREMENT, username varchar(45), authority varchar(45), name varchar(45), PRIMARY KEY (id) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+insert into roles (id, username, authority, name) values (1, 'may', 'ROLE_USER', 'User');
+insert into roles (id, username, authority, name) values (2, 'ple', 'ROLE_ADMIN', 'Admin');
 DROP TABLE users;
 CREATE TABLE users ( id int NOT NULL AUTO_INCREMENT, username varchar(45) NOT NULL, password varchar(255) NOT NULL, enabled tinyint DEFAULT '1' NOT NULL, created_date_time date, updated_date_time bigint, PRIMARY KEY (id), CONSTRAINT ix1 UNIQUE (username) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 insert into users (id, username, password, enabled, created_date_time, updated_date_time) values (1, 'may', '$2a$10$mNHt9RDl.3/ifSOCU44EWeP7lIeMqHy.mYObhX4DRBayPiNvpJU16', 1, null, null);
