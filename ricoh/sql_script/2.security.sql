4273ca16ea02f8c91ac93282082dd6df09d346b1
sarawuti <sarawut.inthong@allianz.com>
2015-10-12 17:50:33 +0700

    update

--- ricoh/sql_script/2.security.sql
+++ ricoh/sql_script/2.security.sql
@@ -3,7 +3,7 @@ CREATE TABLE roles ( id int NOT NULL AUTO_INCREMENT, username varchar(45), autho
 insert into roles (id, username, authority, name) values (1, 'may', 'ROLE_USER', 'User');
 insert into roles (id, username, authority, name) values (2, 'ple', 'ROLE_ADMIN', 'Admin');
 DROP TABLE users;
-CREATE TABLE users ( id int NOT NULL AUTO_INCREMENT, username varchar(45) NOT NULL, password varchar(255) NOT NULL, enabled tinyint DEFAULT '1' NOT NULL, created_date_time date, updated_date_time bigint, PRIMARY KEY (id), CONSTRAINT ix1 UNIQUE (username) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE users ( id int NOT NULL AUTO_INCREMENT, username varchar(45) NOT NULL, password varchar(255) NOT NULL, enabled tinyint DEFAULT '1' NOT NULL, created_date_time datetime, updated_date_time datetime, PRIMARY KEY (id), CONSTRAINT ix1 UNIQUE (username) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 insert into users (id, username, password, enabled, created_date_time, updated_date_time) values (1, 'may', '$2a$10$mNHt9RDl.3/ifSOCU44EWeP7lIeMqHy.mYObhX4DRBayPiNvpJU16', 1, null, null);
 insert into users (id, username, password, enabled, created_date_time, updated_date_time) values (2, 'ple', '$2a$10$mNHt9RDl.3/ifSOCU44EWeP7lIeMqHy.mYObhX4DRBayPiNvpJU16', 1, null, null);
 
@@ -12,5 +12,5 @@ CREATE TABLE `hibernate_sequences` (
   `sequence_name` varchar(255) default NULL,
   `sequence_next_hi_value` int(11) default NULL
 );
-INSERT INTO hibernate_sequences VALUES ('users.id', 2);
-INSERT INTO hibernate_sequences VALUES ('roles.id', 2);
+INSERT INTO hibernate_sequences VALUES ('users.id', 3);
+INSERT INTO hibernate_sequences VALUES ('roles.id', 3);
